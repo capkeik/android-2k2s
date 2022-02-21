97bac7edb421e66bdef9870af759264c22583ba8
capkeik <new.mail2018@mail.ru>
2022-02-21 10:58:58 +0300

    initial commit

---
+++ app/src/main/java/com/example/android_homeworks_2k2s/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_homeworks_2k2s
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
